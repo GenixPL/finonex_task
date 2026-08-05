e768db5a2fc14d500131013d56ee452ff08dad82
Lukasz <intergenixyt@gmail.com>
2026-08-05 23:38:43 +0200

    add reconnecting

--- lib/models/ticker/model/ticker_model_impl.dart
+++ lib/models/ticker/model/ticker_model_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:finonex_task/models/ticker/model/ticker_data.dart';
 import 'package:finonex_task/models/ticker/model/ticker_model.dart';
 import 'package:finonex_task/models/ticker/service/ticker_service.dart';
@@ -6,19 +8,37 @@ class TickerModelImpl extends TickerModel {
   TickerModelImpl({
     required this._service,
   });
-  
+
   // TODO(genix): add throttling (if we have given sub it should mark as needing emit and periodic timer should then emit all updates)
 
   final TickerService _service;
 
-  Stream<TickerData>? _sharedStream;
+  StreamController<TickerData>? _controller;
 
   @override
   Stream<TickerData>? getTickerStream(String symbol) {
-    final shared = _sharedStream ??= Stream.fromFuture(
-      _service.getTickerDataStream(),
-    ).asyncExpand((stream) => stream).asBroadcastStream();
+    if (_controller == null) {
+      _controller = StreamController<TickerData>.broadcast();
+      _startStreaming();
+    }
 
-    return shared.where((ticker) => ticker.symbol == symbol);
+    return _controller!.stream.where((ticker) => ticker.symbol == symbol);
+  }
+
+  Future<void> _startStreaming() async {
+    while (true) {
+      try {
+        final stream = await _service.getTickerDataStream();
+        await for (final data in stream) {
+          _controller?.add(data);
+        }
+      } catch (e) {
+        _controller?.addError(e);
+      }
+
+      // TODO(genix): the 2s are visible, but it works
+      // Wait before reconnecting to avoid tight loops on persistent errors
+      await Future.delayed(const Duration(seconds: 2));
+    }
   }
 }
