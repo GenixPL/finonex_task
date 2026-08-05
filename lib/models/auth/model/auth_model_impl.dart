47de4a159a41384349d75edf4b54821e6cf72aa8
Lukasz <intergenixyt@gmail.com>
2026-08-05 22:11:52 +0200

    getToken

--- lib/models/auth/model/auth_model_impl.dart
+++ lib/models/auth/model/auth_model_impl.dart
@@ -1,4 +1,5 @@
 import 'package:finonex_task/models/_models.dart';
+import 'package:synchronized/synchronized.dart';
 
 class AuthModelImpl extends AuthModel {
   AuthModelImpl({
@@ -9,8 +10,26 @@ class AuthModelImpl extends AuthModel {
   final AuthStorage _authStorage;
   final AuthService _authService;
 
+  final Lock _getTokenLock = Lock();
+
   @override
-  Future<String?> getToken() async {
-    final TokenData? storedData = await _authStorage.getToken();
+  Future<String?> getToken() {
+    return _getTokenLock.synchronized(() async {
+      final TokenData? storedData = await _authStorage.getToken();
+      if (storedData != null && !storedData.isExpired) {
+        return storedData.token;
+      }
+
+      // In this case logged out and expired is the same.
+      // TODO(genix): try-catch
+      final LoginResponse loginResponse = await _authService.login('trader', 'password123');
+      await _authStorage.setToken(
+        TokenData(
+          token: loginResponse.token,
+          expiresIn: loginResponse.expiresIn,
+        ),
+      );
+      return loginResponse.token;
+    });
   }
 }
