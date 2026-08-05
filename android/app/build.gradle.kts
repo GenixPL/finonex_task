d82db2db6f4070a8eb3a34357fae36836fff4abf
Lukasz <intergenixyt@gmail.com>
2026-08-06 00:20:47 +0200

    gen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.example.finonex_task"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,6 +32,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    implementation("androidx.security:security-crypto:1.1.0")
 }
 
 kotlin {
--- android/app/src/main/kotlin/com/example/finonex_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/finonex_task/MainActivity.kt
@@ -3,13 +3,46 @@ package com.example.finonex_task
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+    private lateinit var secureStorageManager: SecureStorageManager
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
+        secureStorageManager = SecureStorageManager(this)
+
         EventChannel(
             flutterEngine.dartExecutor.binaryMessenger,
             "com.example.finonex_task/connectivity",
         ).setStreamHandler(ConnectivityManager(this))
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.example.finonex_task/secure_storage"
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getKey" -> {
+                    val key = call.argument<String>("key")
+                    if (key != null) {
+                        result.success(secureStorageManager.get(key))
+                    } else {
+                        result.error("INVALID_ARGUMENT", "Key is null", null)
+                    }
+                }
+                "setKey" -> {
+                    val key = call.argument<String>("key")
+                    val value = call.argument<String>("value")
+                    if (key != null && value != null) {
+                        secureStorageManager.set(key, value)
+                        result.success(null)
+                    } else {
+                        result.error("INVALID_ARGUMENT", "Key or value is null", null)
+                    }
+                }
+                else -> result.notImplemented()
+            }
+        }
     }
 }
